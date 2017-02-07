ef47112173c80276c4c2ed45401966609649a4ca
hatelove <jay.hatelove@gmail.com>
2017-02-07 21:18:25 +0800

    initialized and add first failed test case: input 1 should return -1;

---
+++ NextBiggerNumber/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NextBiggerNumber
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const int NoLargerNumber = -1;
+
+        [TestMethod]
+        public void Test_1_should_be_largestNumber()
+        {
+            var input = 1;
+
+            var expected = NoLargerNumber;
+
+            int actual = NextLargerNumber.Next(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public static class NextLargerNumber
+    {
+        public static int Next(int input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
