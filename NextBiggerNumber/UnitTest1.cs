015a60ca555dfde76976173ed9a62cd77476ec93
hatelove <jay.hatelove@gmail.com>
2017-02-07 21:41:31 +0800

    add a passed test case: input = 9 should return -1

--- NextBiggerNumber/UnitTest1.cs
+++ NextBiggerNumber/UnitTest1.cs
@@ -19,13 +19,25 @@ namespace NextBiggerNumber
 
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void Test_9_should_be_largestNumber()
+        {
+            var input = 9;
+
+            var expected = NoLargerNumber;
+
+            int actual = NextLargerNumber.Next(input);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     public static class NextLargerNumber
     {
         public static int Next(int input)
         {
-            throw new NotImplementedException();
+            return -1;            
         }
     }
 }
